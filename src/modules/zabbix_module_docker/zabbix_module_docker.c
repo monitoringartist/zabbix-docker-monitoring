9735c33b3b82b87f98b11b8349d1deb8c3a9407c
Jan Garaj <jan.garaj@gmail.com>
2016-02-17 23:08:16 +0000

    Zabbix 3.0 fix

--- src/modules/zabbix_module_docker/zabbix_module_docker.c
+++ src/modules/zabbix_module_docker/zabbix_module_docker.c
@@ -17,8 +17,8 @@
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 **/
 
-#include "log.h"
 #include "common.h"
+#include "log.h"
 #include "comms.h"
 #include "module.h"
 #include "sysinc.h"
