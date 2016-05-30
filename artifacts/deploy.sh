6ce400e33d2937e73e647bda5b995302677930cb
Jan Garaj <jan.garaj@gmail.com>
2016-05-30 18:10:18 +0100

    Travis

--- artifacts/deploy.sh
+++ artifacts/deploy.sh
@@ -75,7 +75,7 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 pwd
 ls -lah
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in artifacts/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../artifacts/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
