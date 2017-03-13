f155e98988bc6d9c31c8444ac8ed63ebc661e2f0
Jan Garaj <jan.garaj@gmail.com>
2017-03-13 02:54:00 +0000

    Fix vstatus metrics

--- src/modules/zabbix_module_docker/zabbix_module_docker.c
+++ src/modules/zabbix_module_docker/zabbix_module_docker.c
@@ -2347,7 +2347,8 @@ int     zbx_module_docker_vstatus(AGENT_REQUEST *request, AGENT_RESULT *result)
             struct zbx_json_parse	jp_data, jp_data2;
             jp_data.start = &answer[0];
             jp_data.end = &answer[strlen(answer)];
-            if (SUCCEED != zbx_json_brackets_by_name(&jp_data, "Volumes", &jp_data2)) {
+
+            if (SUCCEED == zbx_json_brackets_by_name(&jp_data, "Volumes", &jp_data2)) {
                 int count = zbx_json_count(&jp_data2);
                 free((void*) answer);
                 zabbix_log(LOG_LEVEL_DEBUG, "Count of volumes in %s status: %d", state, count);
@@ -2371,14 +2372,17 @@ int     zbx_module_docker_vstatus(AGENT_REQUEST *request, AGENT_RESULT *result)
                 return SYSINFO_RET_FAIL;
             }
 
-            struct zbx_json_parse	jp_data;
+            struct zbx_json_parse	jp_data, jp_data2;
             jp_data.start = &answer[0];
             jp_data.end = &answer[strlen(answer)];
-            int count = zbx_json_count(&jp_data);
-            free((void*) answer);
-            zabbix_log(LOG_LEVEL_DEBUG, "Count of volumes in %s status: %d", state, count);
-            SET_UI64_RESULT(result, count);
-            return SYSINFO_RET_OK;
+
+            if (SUCCEED == zbx_json_brackets_by_name(&jp_data, "Volumes", &jp_data2)) {
+                int count = zbx_json_count(&jp_data2);
+                free((void*) answer);
+                zabbix_log(LOG_LEVEL_DEBUG, "Count of volumes in %s status: %d", state, count);
+                SET_UI64_RESULT(result, count);
+                return SYSINFO_RET_OK;
+            }
         }
 
         zabbix_log(LOG_LEVEL_DEBUG, "Not supported volume state: %s", state);
